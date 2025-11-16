27c384cf8718658490154bd622eba4f9985d68a6
gosang <gosang@outlook.com>
2025-11-16 12:25:23 +0000

    update endpoints DI

--- src/MinimalClean.Api/Program.cs
+++ src/MinimalClean.Api/Program.cs
@@ -1,10 +1,11 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using MinimalClean.Api.Common;
 using MinimalClean.Api.Endpoints.Orders.Create;
 using MinimalClean.Api.Endpoints.Orders.GetById;
 using MinimalClean.Api.Middleware;
 using MinimalClean.Infrastructure.Persistence;
 using MinimalClean.Infrastructure.Persistence.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +26,10 @@ builder.Services.AddScoped<IValidator<CreateOrderRequest>, CreateOrderValidator>
 // Middleware
 builder.Services.AddTransient<ErrorHandlingMiddleware>();
 
+// Register endpoints
+builder.Services.AddScoped<IEndpoint, CreateOrderEndpoint>();
+builder.Services.AddScoped<IEndpoint, GetOrderByIdEndpoint>();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -43,20 +48,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Route groups (optional, for versioning or grouping)
-var orders = app.MapGroup("/orders");
+// Map endpoints dynamically
+foreach (var endpoint in app.Services.GetRequiredService<IEnumerable<IEndpoint>>())
+{
+    endpoint.MapEndpoint(app);
+}
 
-// Create order
-orders.MapPost("", CreateOrderEndpoint.Handler)
-      .WithName("CreateOrder")
-      .Produces(201)
-      .Produces(400);
+//// static approach
+//// Route groups (optional, for versioning or grouping)
+//var orders = app.MapGroup("/orders");
 
-// Get by id
-orders.MapGet("{id:guid}", GetOrderByIdEndpoint.Handler)
-      .WithName("GetOrderById")
-      .Produces(200)
-      .Produces(404);
+//// Create order
+//orders.MapPost("", CreateOrderEndpoint.Handler)
+//      .WithName("CreateOrder")
+//      .Produces(201)
+//      .Produces(400);
+
+//// Get by id
+//orders.MapGet("{id:guid}", GetOrderByIdEndpoint.Handler)
+//      .WithName("GetOrderById")
+//      .Produces(200)
+//      .Produces(404);
 
 var summaries = new[]
 {
