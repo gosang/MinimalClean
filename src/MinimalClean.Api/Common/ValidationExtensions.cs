c8171b5050b15a4b25eb32431ec3a13aebebfb9d
gosang <gosang@outlook.com>
2025-11-16 11:53:58 +0000

    add ValidationExtensions

---
+++ src/MinimalClean.Api/Common/ValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace MinimalClean.Api.Common;
+
+public static class ValidationExtensions
+{
+    public static IResult ToBadRequest(this ValidationResult result) =>
+        Results.BadRequest(new
+        {
+            errors = result.Errors.Select(e => new { e.PropertyName, e.ErrorMessage })
+        });
+}
