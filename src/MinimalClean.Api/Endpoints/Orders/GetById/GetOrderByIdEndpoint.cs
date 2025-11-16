cb5beb271b1d3d62cd60aaff8c4f14f0981d3cf1
gosang <gosang@outlook.com>
2025-11-16 12:03:24 +0000

    add GetOrderByIdEndpoint

---
+++ src/MinimalClean.Api/Endpoints/Orders/GetById/GetOrderByIdEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using MinimalClean.Api.Common;
+
+namespace MinimalClean.Api.Endpoints.Orders.GetById;
+
+public class GetOrderByIdEndpoint : EndpointBase
+{
+    public static string Pattern => "/orders/{id:guid}";
+    public static Delegate Handler =>
+        async (Guid id, GetOrderByIdHandler handler, CancellationToken ct) =>
+        {
+            var result = await handler.Handle(new GetOrderByIdQuery(id), ct);
+            return result is null ? NotFound($"Order {id} not found.") : Ok(result);
+        };
+}
