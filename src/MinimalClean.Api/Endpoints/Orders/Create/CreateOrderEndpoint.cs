d9b2830289a9685b6bd5f9fe974bc643cc70a748
gosang <gosang@outlook.com>
2025-11-16 11:59:45 +0000

    add CreateOrderEndpoint

---
+++ src/MinimalClean.Api/Endpoints/Orders/Create/CreateOrderEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MinimalClean.Api.Common;
+
+namespace MinimalClean.Api.Endpoints.Orders.Create;
+
+public class CreateOrderEndpoint : EndpointBase
+{
+    public static string Pattern => "/orders";
+    public static Delegate Handler =>
+        async (CreateOrderRequest req,
+               IValidator<CreateOrderRequest> validator,
+               CreateOrderHandler handler,
+               HttpContext ctx,
+               CancellationToken ct) =>
+        {
+            var validation = await validator.ValidateAsync(req, ct);
+            if (!validation.IsValid) return validation.ToBadRequest();
+
+            var id = await handler.Handle(new CreateOrderCommand(req.CustomerName, req.Total), ct);
+            var uri = $"{ctx.Request.Scheme}://{ctx.Request.Host}{Pattern}/{id}";
+            return Created(uri, new { id });
+        };
+}
