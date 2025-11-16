571bf2c0453c910864e6f70472a92423b996b5ca
gosang <gosang@outlook.com>
2025-11-16 11:52:52 +0000

    add endpointbase

---
+++ src/MinimalClean.Api/Common/EndpointBase.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalClean.Api.Common;
+
+public abstract class EndpointBase
+{
+    protected static IResult Ok<T>(T value) => Results.Ok(value);
+    protected static IResult Created<T>(string uri, T value) => Results.Created(uri, value);
+    protected static IResult NotFound(string message) => Results.NotFound(new { error = message });
+    protected static IResult BadRequest(object error) => Results.BadRequest(error);
+}
